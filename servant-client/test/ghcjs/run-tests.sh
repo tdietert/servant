769fde4799921789e89b4c1e229feb13751c1690
Sönke Hahn <soenkehahn@gmail.com>
2016-04-22 18:18:33 +0800

    wip

--- servant-client/test/ghcjs/run-tests.sh
+++ servant-client/test/ghcjs/run-tests.sh
@@ -1,11 +1,12 @@
 #/usr/bin/env bash
 
-# this script has to be executed from the 'servant-client' directory
-
 set -o errexit
+
+loc=$(pwd)/$(dirname $0)
+cd $loc
 
 npm install xhr2
 
-export STACK_YAML=stack-ghcjs.yaml
+export STACK_YAML=../../stack-ghcjs.yaml
 stack setup
 stack test --fast
--- travis.sh
+++ travis.sh
@@ -3,7 +3,13 @@
 set -o errexit
 
 if [ "$GHCJS" = "true" ]; then
-  (cd servant-client && ./test/ghcjs/run-tests.sh)
+  # fixme: provide more specific image tag
+  IMAGE=soenkehahn/ghcjs-for-travis:latest
+  IMAGE=soenkehahn/docker-test:latest
+  # docker pull $IMAGE
+  # docker run --rm -v $(pwd):/root/servant $IMAGE ls -la /root
+  # docker run --rm -v $(pwd):/root/servant $IMAGE ls -la /root
+  ./servant-client/test/ghcjs/run-tests.sh
 else
   for package in $(cat sources.txt) doc/tutorial ; do
     echo testing $package
